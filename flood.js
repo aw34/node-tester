f35721940cc233c867c27ed1f5e5b37227c91719
Alex <alex@ubuntu.ubuntu-domain>
2012-04-11 00:31:26 -0700

    ...

--- flood.js
+++ flood.js
@@ -1,14 +1,26 @@
 var http = require('http');
 var hrtime = require('hrtime');
 var fs =require('fs');
-var userinput = process.argv.slice(2);
+//var stream = fs.createWriteStream('output.txt', {'flags':'a'});
+var userinput = [];
 var numberoftest;
 var stop;
 var globalStartTime;
 var counter =0;
 var reqinc;
 var intinc;
-
+//var numberofrequest =userinput[0];;
+//var intervaltime= userinput[1];
+//var latarray=[];
+//var startarray=[];
+//var badrequest=0;
+//var latarrayindex=0;
+//var total=0;
+//var mean=0;
+//var sd=0;
+//var globalStartTime = hrtime.time();
+//var reqPerSecond=0;
+//var testresult;
 var resultarray= [];
 var io = require('socket.io').listen(8001);
 
@@ -85,7 +97,9 @@ setInterval(interval, t);
 };
 
 function testnowait(n){
-
+//for (var i =0; i< n; i++){
+// temp(i, latarray);
+//};
 var counter = 0;
 while (counter < n){
 temp(counter, latarray);
@@ -107,7 +121,7 @@ array.push(latency);
 res.on("end", function() {
 if (array.length==testresult.numberofrequest-testresult.badrequest){
 endoftest(array);
-
+//res.emit('finish',endoftest(array));
 };
 })
 
@@ -115,7 +129,10 @@ endoftest(array);
 });
 req.on('error', function(e) {
 console.log('Request #',k,': ERROR',e.message);
-
+//testresult.badrequest++;
+//console.log('test array length',array.length);
+//console.log(testresult.numberofrequest);
+//console.log(testresult.badrequest);
 if (array.length==testresult.numberofrequest-testresult.badrequest){
 endoftest(array);
 //req.emit('finish',endoftest(array));
@@ -143,8 +160,11 @@ testresult.mean = calMean(testresult.total,array.length);
 //testresult.sd = calSD(array,testresult.mean);
 
 console.log('Total Latency =',Math.round(testresult.total/1000000)/1000,'seconds');
+//console.log(total);
 console.log('Mean =',Math.round(testresult.mean/1000)/1000,'milliseconds');
+//console.log(mean);
 //console.log('Standard Deviation =',Math.round(testresult.sd/1000)/1000,'milliseconds');
+//console.log(sd);
 console.log('Number of bad request =',testresult.badrequest);
 output2();
 
@@ -197,14 +217,40 @@ stream.end();
 };
 
 function output2(){
-
+//var stream = fs.createWriteStream('output.txt', {'flags':'a'});
+//var options = {
+//numberofrequest: numberofrequest [
+// testresult:{
+// numberofrequest: numberofrequest,
+// total:total,
+// mean: mean,
+// sd: sd,
+// badrequest: badrequest
+// }
+//};
+//var data = JSON.stringify(options, null,2);
+//console.log('SUMMARY SAVED TO OUTPUT.TXT');
+//stream.write('{\n');
+//stream.write('"testresult": {\n');
+//stream.write(Math.round(testresult.reqPerSecond)+'\n');
+//stream.write((testresult.numberofrequest)+'\n');
+//stream.write((testresult.mean)+'\n');
+//stream.write((Math.round(testresult.testEndTime/1000000)/1000)+'\n');
+//stream.end();
 var data=[];
   data[0]=testresult.reqPerSecond;
   data[1]=testresult.numberofrequest;
   data[2]=testresult.mean;
   data[3]=Math.round(testresult.testEndTime/1000000)/1000;
+ // io.sockets.emit("Ndata", {newdata:"New data is coming..."});
   io.sockets.emit("all", data);
- 
+  //io.sockets.emit("cat", Math.round(testresult.reqPerSecond));
+  //io.sockets.emit("tol", testresult.numberofrequest);
+  //io.sockets.emit("ser", testresult.mean);
+  //io.sockets.emit("sec", Math.round(testresult.testEndTime/1000000)/1000);
+  
+//});
+
 run();
 };
 
@@ -247,11 +293,21 @@ temp = Math.sqrt(temp);
 return temp;
 };
 
+//function run(){
 
+//setup();
+
+//test(resultarray[0].numberofrequest,resultarray[0].intervaltime);
+//};
 
 function run(){
-if (numberoftest < stop){//run another test
+if (numberoftest < stop){
 setup();
+//console.log(testresult.numberofrequest);
+//console.log('numberoftest',numberoftest);
+//console.log(numberoftest*10);
+//testresult.numberofrequest = testresult.numberofrequest*1+(numberoftest*userinput[0]);
+//testresult.intervaltime = testresult.intervaltime*1-Math.round((numberoftest*userinput[1]/10));
 
 testresult.numberofrequest = testresult.numberofrequest*1+(numberoftest*recinc);
 testresult.intervaltime = testresult.intervaltime*1-Math.round((numberoftest*intinc));
@@ -259,8 +315,8 @@ testresult.intervaltime = testresult.intervaltime*1-Math.round((numberoftest*int
 if (testresult.intervaltime<1){testresult.intervaltime=1};
 
 test(resultarray[resultarray.length-1].numberofrequest,resultarray[resultarray.length-1].intervaltime);
-} else { //finish
-
+} else {
+//process.exit();
 console.log('END OF FLOOD');
 counter++;
 io.sockets.emit("floodIsDone", {newstatus:"Test is finished"});
@@ -271,26 +327,88 @@ io.sockets.emit("floodIsDone", {newstatus:"Test is finished"});
 };
 };
 
+//run();
+
+//if (userinput.length == 1){
+//console.log('no wait');
+// testnowait(numberofrequest);
+//}else {
+//test(numberofrequest,intervaltime);
+//};
+//console.log(testresult.badrequest);
+
+
+//setup();
+//resultarray.push(testresult);
+//testresult.badrequest = 100;
+//setup();
+//resultarray.push(testresult);
+//testresult.badrequest = 110;
+//setup();
+//resultarray.push(testresult);
+//setup();
+//resultarray.push(testresult);
+//testresult;
+//setup();
+//resultarray.push(testresult);
+//setup()
+/*
+resultarray[0].badrequest=0;
+resultarray[1].badrequest=1;
+resultarray[2].badrequest=2;
+resultarray[3].badrequest=3;
+resultarray[4].badrequest=4;
+//testresult.badrequest = 100000000;
+console.log(resultarray[0].badrequest);
+console.log(resultarray[1].badrequest);
+console.log(resultarray[2].badrequest);
+console.log(resultarray[3].badrequest);
+console.log(resultarray[4].badrequest);
+console.log(testresult.badrequest);
+console.log('end of setup test');
+
+
+//console.log(testresult.badrequest);
+//resultarray.push(testresult);
+//console.log(resultarray[5].badrequest);
+//testresult.badrequest = 10000;
+
+//console.log(resultarray[0]);
+//console.log(resultarray[1]);
+//console.log(resultarray[2]);
+//console.log(resultarray[3]);
+//console.log(resultarray[4]);
+//testresult.badrequest = 100;
+//console.log('testing after');
+//console.log(testresult.badrequest);
+//console.log(resultarray[0].badrequest);
+//console.log(resultarray[1].badrequest);*/
+
 io.sockets.on('connection', function (socket) {
-            socket.on('message', function(message){
-               
-                  if (message == "linear")
-                  {  
+            socket.on('linear', function(data){
                       socket.emit("update", {newstatus:"Low load testing..."});
+                      console.log(data[0]);
+                                            console.log(data[1]);
+
+                      userinput[0]=data[0];
+                      userinput[1]=data[1];
                       recinc=userinput[0];
                       intinc=userinput[1]/10;
                       init();
                       run();                     
                      //console.log("fabonacci(15) = "+n+"--from client");
-                     
-                  }else if (message == "log")
-                  {
+            });         
+            socket.on('log', function(data){
+                
                       socket.emit("update", {newstatus:"High load testing..."});
+                      userinput[0]=data[0];
+                      userinput[1]=data[1];
                       recinc=userinput[0]*1.5;
                       intinc=userinput[1]/8;
                       init();
                       run(); 
-                  }
+                  
                });
             });
+//pre-entered host options
 
